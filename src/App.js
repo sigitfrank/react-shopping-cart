378a40c04858472e0cc00005604f7c68614eb82e
sigitfrank <sigittt53@gmail.com>
2021-03-22 15:26:15 +0700

    add items to cart, remove cart, add sidebar cart

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Products from './components/Products'
 import Filter from './components/Filter'
 import data from './data.json'
+import Cart from './components/Cart'
 
 class App extends React.Component {
 
@@ -9,15 +10,35 @@ class App extends React.Component {
     super()
     this.state = {
       products: data.products,
+      cartItems: [],
       size: "",
       sort: ""
     }
   }
 
+  removeFromCart = (product) => {
+    const cartItems = this.state.cartItems.slice()
+    this.setState({ cartItems: cartItems.filter(item => item._id !== product._id) })
+  }
+
+  addToCart = (product) => {
+    const cartItems = this.state.cartItems.slice()
+    let alreadyInCart = false
+    cartItems.forEach(item => {
+      if (item._id === product._id) {
+        item.count++
+        alreadyInCart = true
+      }
+    })
+    if (!alreadyInCart) {
+      cartItems.push({ ...product, count: 1 })
+    }
+    this.setState({ cartItems })
+  }
+
   sortProducts = (event) => {
     const sort = event.target.value
-    console.log(sort);
-    this.setState((state) => ({
+    this.setState(() => ({
       sort: sort,
       products: this.state.products.slice().sort((a, b) =>
         sort === "lowest"
@@ -58,11 +79,11 @@ class App extends React.Component {
                 sortProducts={this.sortProducts}>
 
               </Filter>
-              <Products products={this.state.products}></Products>
+              <Products products={this.state.products} addToCart={this.addToCart}></Products>
             </div>
 
             <div className="sidebar">
-              Cart Items
+              <Cart cartItems={this.state.cartItems} removeFromCart={this.removeFromCart} />
             </div>
           </div>
         </main>
--- src/components/Products.js
+++ src/components/Products.js
@@ -18,7 +18,7 @@ export default class Products extends Component {
                                     <div>
                                         {formatCurrency(product.price)}
                                     </div>
-                                    <button className="button primary">
+                                    <button className="button primary" onClick={() => this.props.addToCart(product)}>
                                         Add To Cart
                                     </button>
                                 </div>
